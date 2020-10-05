78934bc47445586c68ddf9b40ca2cf192a9f9632
Don Charles-Lambert <zcabdc0@ucl.ac.uk>
2020-10-05 20:08:49 +0100

    Build in root directory to support CI

---
+++ precache-manifest.53a0799a7139f3dd9b26a657ba984b1f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ba057890fec3855b35b47558abf3b0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2f2e64f172eb2b540ba",
+    "url": "/static/css/main.4d9ceb09.chunk.css"
+  },
+  {
+    "revision": "8a569b9ac57948aa5670",
+    "url": "/static/js/2.55776283.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.55776283.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2f2e64f172eb2b540ba",
+    "url": "/static/js/main.54689711.chunk.js"
+  },
+  {
+    "revision": "a22f1a45f8893f21fa97",
+    "url": "/static/js/runtime-main.a36e78d1.js"
+  },
+  {
+    "revision": "e396bbeda4f616f2f55c3bed7b9f52aa",
+    "url": "/static/media/Algorithms To Live By.e396bbed.jpg"
+  },
+  {
+    "revision": "0150a9519159e164cf98edf8913dd751",
+    "url": "/static/media/Computer Science - An Overview.0150a951.jpg"
+  },
+  {
+    "revision": "a92d81127400937aac7cd1786600700d",
+    "url": "/static/media/Golden Ticket.a92d8112.jpg"
+  },
+  {
+    "revision": "eb3a10c333d48ef2e94b7e595bd37750",
+    "url": "/static/media/Introducing Logic.eb3a10c3.jpg"
+  },
+  {
+    "revision": "f7078e52d202a73eb723dac6a8f62029",
+    "url": "/static/media/Mind Design II.f7078e52.jpg"
+  },
+  {
+    "revision": "0c3856a335fdfdb8b0652f7c3d367fdd",
+    "url": "/static/media/Moderat.0c3856a3.ttf"
+  },
+  {
+    "revision": "838d005c878ef62cca8b63be57f6325b",
+    "url": "/static/media/SuperIntelligence.838d005c.jpg"
+  },
+  {
+    "revision": "d55caa9d3866f9ce23abbe62aea5252e",
+    "url": "/static/media/black-cube-bg.d55caa9d.jpg"
+  }
+]);
