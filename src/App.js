dbaf92d5a883bb3cc6e6b0956d9d42b0b0690517
Don Charles-Lambert <zcabdc0@ucl.ac.uk>
2020-10-05 22:14:47 +0100

    Minor design changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Modal from 'react-modal'
 import './style/App.css';
 import Overview from './img/Computer Science - An Overview.jpg'
 import Logic from './img/Introducing Logic.jpg'
@@ -6,11 +7,11 @@ import SuperIntelligence from './img/SuperIntelligence.jpg'
 import Algorithms from './img/Algorithms To Live By.jpg'
 import Ticket from './img/Golden Ticket.jpg'
 import Design from './img/Mind Design II.jpg'
+import Washington from './img/head-washington.gif'
 
 function App() {
   return (
     <div className="App" style={{height: window.innerHeight}}>
-      <Menu/>
       <Intro/>
       <QuizList/>
       <BookPopUp/>
@@ -31,9 +32,12 @@ function Menu(){
 function Intro(){
   return(
     <div>
-      <h1 className="main-heading"> DAWN OF KNOWLEDGE </h1>
-      <p className="main-paragraph">
-      </p>
+      <div style={{position: 'relative'}}>
+        <img src={Washington} className='logo'/>  
+        <h1 className="main-heading">
+          DAWN OF KNOWLEDGE
+        </h1>
+      </div>
     </div>
   )
 }
@@ -41,12 +45,36 @@ function Intro(){
 function QuizList(props){
   return(
     <div className="quiz-list" style={{height: 0.75 * window.innerHeight}}>
-      <QuizItem img={Overview} title={'Computer Science: An Overview'} description={'This book presents an introductory survey of computer science.'}/>
-      <QuizItem img={Logic} title={'Introducing Logic'} description={'Logic is the backbone of Western civilization, holding together its systems of philosophy, science and law.'}/>
-      <QuizItem img={SuperIntelligence} title={'Superintelligence'} description={'New superintelligence could replace humans as the dominant lifeform on Earth.'}/>
-      <QuizItem img={Algorithms} title={'Algorithms To Live By'} description={'A fascinating exploration of how computer algorithms can be applied to our everyday lives.'}/>
-      <QuizItem img={Ticket} title={'Golden Ticket'} description={'Provides a nontechnical introduction to P-NP, its rich history, and its algorithmic implications'}/>
-      <QuizItem img={Design} title={'Mind Design II'} description={'Mind design is the endeavor to understand mind (thinking, intellect) in terms of its design (how it is built, how it works).'}/>
+      <QuizItem
+        img={Overview}
+        title={'Computer Science: An Overview'}
+        description={'This book presents an introductory survey of computer science.'}
+      />
+      <QuizItem
+        img={Logic}
+        title={'Introducing Logic'}
+        description={'Logic is the backbone of Western civilization, holding together its systems of philosophy, science and law.'}
+      />
+      <QuizItem 
+        img={SuperIntelligence}
+        title={'Superintelligence'}
+        description={'New superintelligence could replace humans as the dominant lifeform on Earth.'}
+      />
+      <QuizItem
+        img={Algorithms}
+        title={'Algorithms To Live By'}
+        description={'A fascinating exploration of how computer algorithms can be applied to our everyday lives.'}
+      />
+      <QuizItem
+        img={Ticket}
+        title={'Golden Ticket'}
+        description={'Provides a nontechnical introduction to P-NP, its rich history, and its algorithmic implications'}
+      />
+      <QuizItem
+        img={Design}
+        title={'Mind Design II'} 
+        description={'Mind design is the endeavor to understand mind (thinking, intellect) in terms of its design (how it is built, how it works).'}
+      />
     </div>
   )
 }
@@ -69,7 +97,7 @@ class QuizItem extends React.Component{
     return(
       <div className="item" style={{height: this.state.itemHeight, transition: '2s'}} onClick={this.expand}>
         <div style={{display: 'flex', height: '100px'}}>
-          <img src={this.props.img} className='item-img' alt={this.props.title}/>
+          <img src={this.props.img} style={{transition: '2s'}} className='item-img' alt={this.props.title}/>
           <div style={{alignSelf: 'center', width: '100%', padding: '5px'}}>
             <h3 className='item-title m-0'>{this.props.title}</h3>
             <p className='item-description m-0'>{this.props.description}</p>
@@ -90,10 +118,10 @@ class QuizItem extends React.Component{
 
 function BookPopUp(props){
   return(
-    <div className='book-popup'>
+    <Modal className='book-popup'>
       <img src={props.img} alt={props.title}/>
       <p className='item-description m-0'>{props.description}</p>
-    </div>
+    </Modal>
   )
 }
 
